355b7107a39c8425c1038529f80c94dc9eccad54
Donald Hunter <donald.hunter@gmail.com>
2023-09-21 15:26:46 +0100

    Implement a generic bpf_hw_ndo_setup_tc to register for callbacks
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

--- net/bpf/netdev_hw.c
+++ net/bpf/netdev_hw.c
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#include "asm-generic/errno.h"
 #include <linux/bpf_verifier.h>
 #include <linux/bpf.h>
 #include <linux/btf.h>
 #include <linux/printk.h>
 #include <linux/netdev_hw.h>
+#include <net/flow_offload.h>
 
 extern struct bpf_struct_ops bpf_net_device_hw_ops;
 
@@ -45,13 +47,19 @@ static int bpf_hw_reg(void *kdata, struct bpf_struct_ops_link *link)
 {
 	struct net_device *dev = bpf_struct_ops_link_get_device(link);
 	printk(KERN_INFO "bpf_hw_reg: %s\n", dev->name);
+	dev->hw_ops = kdata;
 	return 0;
 }
 
 static void bpf_hw_unreg(void *kdata, struct bpf_struct_ops_link *link)
 {
 	struct net_device *dev = bpf_struct_ops_link_get_device(link);
-	printk(KERN_INFO "bpf_hw_unreg: %s\n", dev->name);
+	if (dev->hw_ops == kdata) {
+		dev->hw_ops = NULL;
+		printk(KERN_INFO "bpf_hw_unreg: %s successful\n", dev->name);
+	} else {
+		printk(KERN_INFO "bpf_hw_unreg: %s failed\n", dev->name);
+	}
 }
 
 static int bpf_hw_update(void *kdata, void *old_kdata)
@@ -115,3 +123,36 @@ struct bpf_struct_ops bpf_net_device_hw_ops = {
 	.validate = bpf_hw_validate,
 	.name = "net_device_hw_ops",
 };
+
+
+static LIST_HEAD(bpf_hw_block_ft_cb_list);
+int bpf_hw_setup_tc(struct net_device *dev, enum tc_setup_type type,
+		    void *type_data)
+{
+	const struct net_device_hw_ops *hw_ops = dev->hw_ops;
+	void *priv = netdev_priv(dev);
+
+	if (!hw_ops) {
+		printk(KERN_INFO "No hw_ops registered with %s\n", dev->name);
+		return -EOPNOTSUPP;
+	}
+
+	switch (type) {
+	case TC_SETUP_FT:
+		if (!hw_ops->setup_ft) {
+			printk(KERN_INFO "No setup_tc to call in hw_ops on %s\n",
+			       dev->name);
+			return -EOPNOTSUPP;
+		}
+		printk(KERN_INFO "Calling flow_block_cb_setup_simple on %s\n",
+		       dev->name);
+		return flow_block_cb_setup_simple(type_data,
+						  &bpf_hw_block_ft_cb_list,
+						  hw_ops->setup_ft,
+						  priv, priv, true);
+	default:
+		return -EOPNOTSUPP;
+	}
+}
+
+EXPORT_SYMBOL(bpf_hw_setup_tc);
