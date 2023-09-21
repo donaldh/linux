dc8a66f0048a1b4a116303d9aa80176d485aca44
Donald Hunter <donald.hunter@gmail.com>
2023-09-21 15:25:09 +0100

    Add setup_tc and setup_ft to netdev struct_ops
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

--- include/linux/netdev_hw.h
+++ include/linux/netdev_hw.h
@@ -3,11 +3,20 @@
 #define _LINUX_NETDEV_HW_H
 
 #include <linux/netdevice.h>
+#include <net/flow_offload.h>
 
 struct net_device_hw_ops {
 	void (*offload)(struct net_device *dev,
 			struct flow_cls_offload *off);
+	int (*setup_tc)(struct net_device *dev,
+			int type,
+			void *type_data);
+	int (*setup_ft)(enum tc_setup_type type,
+			void *type_data,
+			void *cb_priv);
 	char name[16];
 };
+
+int bpf_hw_setup_tc(struct net_device *dev, enum tc_setup_type type, void *type_data);
 
 #endif /* _LINUX_NETDEV_HW_H */
